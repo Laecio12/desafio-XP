6e9d5c2bd8479920484baea12898034bb83eeff2
laecio silva <laecio12silva@gmail.com>
2022-07-16 18:50:59 -0300

    refactor: Tratando erros no catch com AppError.

--- src/models/User/index.js
+++ src/models/User/index.js
@@ -1,12 +1,13 @@
+import AppError from '../../errors/AppError.js';
 import connection from '../connection.js';
 
 const create = async (id, name, cpf, password) => {
   try {
     await connection.execute('INSERT INTO users (id, name, cpf, password) VALUES (?, ?, ?, ?)', [id, name, cpf, password]);
+    return { id };
   } catch (error) {
-    console.log(error);
+    throw new AppError(error.message, 500);
   }
-  return { id };
 };
 
 export default { create };
