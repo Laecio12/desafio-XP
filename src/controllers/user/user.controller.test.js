c44b015d1a1aa73393078e83560de7f627e62e85
laecio silva <laecio12silva@gmail.com>
2022-07-16 16:49:49 -0300

    test: Adicionado o test quando o usuário é inserido com sucesso

--- src/controllers/user/user.controller.test.js
+++ src/controllers/user/user.controller.test.js
@@ -4,27 +4,28 @@ import { expect } from 'chai';
 import userService from '../../services/user/index.js';
 import userController from './index.js';
 
-describe("quando é inserido com sucesso", async () => {
+describe('quando um usuário é inserido com sucesso', async () => {
   const response = {};
   const request = {};
 
   before(() => {
     request.body = {
-      "name": "Arco5",
-      "quantity": 100
+      name: 'laecio',
+      cpf: '00000000000',
+      password: '123456',
     };
 
     response.status = sinon.stub().returns(response);
-    response.json = sinon.stub().returns("user criado com sucesso!");
-    sinon.stub(userService, "create").resolves(true);
-   });
+    response.json = sinon.stub().returns('user criado com sucesso!');
+    sinon.stub(userService, 'create').resolves(true);
+  });
 
-   after(() => {
-     userService.create.restore();
-   });
+  after(() => {
+    userService.create.restore();
+  });
 
-   it("é chamado o status com o código 201", async () => {
-     await userController.create(request, response);
-     expect(response.status.calledWith(201)).to.be.equal(true);
+  it('é chamado o status com o código 201', async () => {
+    await userController.create(request, response);
+    expect(response.status.calledWith(201)).to.be.equal(true);
   });
 });
