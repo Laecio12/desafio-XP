098ae25fabaab2fe6533d158db08a73fe8d87148
laecio silva <laecio12silva@gmail.com>
2022-07-16 18:01:31 -0300

    feat: Adicionando a rota /docs para mostrar documentação com o swagger.

--- src/server.js
+++ src/server.js
@@ -1,9 +1,14 @@
 import express from 'express';
 import 'dotenv/config';
+import swaggerJsdoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
 import routes from './routes/index.js';
+import swaggerConfig from './docs/swagger.config.js';
 
 const app = express();
 app.use(express.json());
+const swaggerDoc = swaggerJsdoc(swaggerConfig);
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 app.use(routes);
 
 app.listen(3333, () => console.log('Server started on port 3333'));
