e8b06ea08f54c601cb621dfc552b7c3db02d0105
laecio silva <laecio12silva@gmail.com>
2022-07-16 15:30:15 -0300

    refactor: Alterando o status para 201

--- src/controllers/user/index.js
+++ src/controllers/user/index.js
@@ -3,7 +3,7 @@ import userService from '../../services/user/index.js';
 const create = async (request, response) => {
   const { name, cpf, password } = request.body;
   const user = await userService.create(name, cpf, password);
-  response.status(200).json({ token: user.token });
+  response.status(201).json({ token: user.token });
 };
 
 export default { create };
