dd31e06b439ca78de5ef10a8667297fe8fe4c05a
laecio silva <laecio12silva@gmail.com>
2022-07-16 18:32:20 -0300

    feat: Adicionado o middleware validate a rota do tipo post em users.

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import userController from '../controllers/user/index.js';
+import validate from '../middlewares/user/validate.js';
 
 const userRouter = express.Router();
 
-userRouter.post('/', userController.create);
+userRouter.post('/', validate, userController.create);
 
 export default userRouter;
