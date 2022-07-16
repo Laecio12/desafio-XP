e2cfcfc3efeff14d7334dabfc393dc4a54f4ccdf
laecio silva <laecio12silva@gmail.com>
2022-07-16 19:00:37 -0300

    feat: Verificaçãao se usuário já existe, caso exista retorna um  error com código 409, e messagem "Usuário já existeUsuário já existe".

--- src/services/user/index.js
+++ src/services/user/index.js
@@ -1,7 +1,12 @@
 import { v4 as uuid } from 'uuid';
+import AppError from '../../errors/AppError.js';
 import UserModel from '../../models/User/index.js';
 
 const create = async (name, cpf, password) => {
+  const [userExists] = await UserModel.findByCpf(cpf);
+
+  if (userExists) throw new AppError('Usuário já existe', 409);
+
   const user = await UserModel.create(uuid(), name, cpf, password);
 
   return user;
