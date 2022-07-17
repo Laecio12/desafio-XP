e67e3769edea0c9450af4433264f74a173cac8e3
laecio silva <laecio12silva@gmail.com>
2022-07-17 13:45:19 -0300

    feat: Adicionando aa rota saque.

--- src/routes/accounts.routes.js
+++ src/routes/accounts.routes.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import account from '../controllers/account/index.js';
-import validateDeposit from '../middlewares/account/index.js';
+import validateValue from '../middlewares/account/index.js';
 
 const accountRouter = express.Router();
 
-accountRouter.post('/deposito', validateDeposit, account.deposit);
+accountRouter.post('/deposito', validateValue, account.deposit);
+accountRouter.post('/saque', validateValue, account.withdraw);
 
 export default accountRouter;
