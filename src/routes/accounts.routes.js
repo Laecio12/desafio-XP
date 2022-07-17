a3ae996d8ff91cc50fc19ece66ab03bf57bcea15
laecio silva <laecio12silva@gmail.com>
2022-07-17 11:31:40 -0300

    feat: Adicionando o middleware a rota deposito.

--- src/routes/accounts.routes.js
+++ src/routes/accounts.routes.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import account from '../controllers/account/index.js';
+import validateDeposit from '../middlewares/account/index.js';
 
 const accountRouter = express.Router();
 
-accountRouter.post('/deposito', account.deposit);
+accountRouter.post('/deposito', validateDeposit, account.deposit);
 
 export default accountRouter;
