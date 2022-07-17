846d1212cab368a4881e983c56127c9022f6b698
laecio silva <laecio12silva@gmail.com>
2022-07-17 14:12:07 -0300

    feat: Adicionando a função getBalance.

--- src/services/account/index.js
+++ src/services/account/index.js
@@ -5,10 +5,15 @@ const deposit = async (accountNumber, value) => {
   await AccountModel.deposit(accountNumber, value);
 };
 
+const getBalance = async (accountNumber) => {
+  const [{ balance }] = await AccountModel.getBalance(accountNumber);
+  return balance;
+};
+
 const withdraw = async (accountNumber, value) => {
   const [{ balance }] = await AccountModel.getBalance(accountNumber);
   if (balance < value) throw new AppError('Saldo insuficiente');
   await AccountModel.withdraw(accountNumber, value);
 };
 
-export default { deposit, withdraw };
+export default { deposit, withdraw, getBalance };
