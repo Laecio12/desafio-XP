8d88d0f2c840bbc25fdd222e70dcc2c335e93ec2
laecio silva <laecio12silva@gmail.com>
2022-07-17 11:30:43 -0300

    feat: Middleware para validar o  valor do depósito.

---
+++ src/middlewares/account/index.js
@@ -0,0 +1,11 @@
+import AppError from '../../errors/AppError.js';
+
+const validateDeposit = (req, res, next) => {
+  const { value } = req.body;
+  if (!value) throw new AppError('Valor é obrigatório');
+  if (typeof value === 'string') throw new AppError('Valor informado deve ser um número', 422);
+  if (value < 0) throw new AppError('Valor informado deve ser maior que 0', 422);
+  next();
+};
+
+export default validateDeposit;
