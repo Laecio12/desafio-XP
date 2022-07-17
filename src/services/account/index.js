c8f397b88e40b0df651e5a3a384776f353123df6
laecio silva <laecio12silva@gmail.com>
2022-07-17 13:46:32 -0300

    feat: Adicionando a função withdraw que só permite saque caso o saldo seja maior que 0..

--- src/services/account/index.js
+++ src/services/account/index.js
@@ -1,7 +1,14 @@
+import AppError from '../../errors/AppError.js';
 import AccountModel from '../../models/Account/index.js';
 
 const deposit = async (accountNumber, value) => {
   await AccountModel.deposit(accountNumber, value);
 };
 
-export default { deposit };
+const withdraw = async (accountNumber, value) => {
+  const [{ balance }] = await AccountModel.getBalance(accountNumber);
+  if (balance < value) throw new AppError('Saldo insuficiente');
+  await AccountModel.withdraw(accountNumber, value);
+};
+
+export default { deposit, withdraw };
