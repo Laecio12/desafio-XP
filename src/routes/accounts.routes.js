b03d396da7033cd7458c17b435b222ee497c10f8
laecio silva <laecio12silva@gmail.com>
2022-07-17 14:10:49 -0300

    feat: Adicionando a rota  saldo para obter o saldo.

--- src/routes/accounts.routes.js
+++ src/routes/accounts.routes.js
@@ -6,5 +6,6 @@ const accountRouter = express.Router();
 
 accountRouter.post('/deposito', validateValue, account.deposit);
 accountRouter.post('/saque', validateValue, account.withdraw);
+accountRouter.get('/saldo', account.getBalance);
 
 export default accountRouter;
