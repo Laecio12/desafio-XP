5066255cd222ba3e81a1678ac12937afdf4eca5c
laecio silva <laecio12silva@gmail.com>
2022-07-17 09:42:16 -0300

    feat: Adcionando transaction para só adicionar o usuário caso todas a queries sejam executadas corretamente.

--- src/models/User/index.js
+++ src/models/User/index.js
@@ -1,12 +1,21 @@
 import AppError from '../../errors/AppError.js';
 import connection from '../connection.js';
 
-const create = async (id, name, cpf, password) => {
+const create = async (id, name, cpf, password, accountNumber) => {
+  const transaction = await connection.getConnection();
+
   try {
-    await connection.execute('INSERT INTO users (id, name, cpf, password) VALUES (?, ?, ?, ?)', [id, name, cpf, password]);
-    return { id };
+    await transaction.beginTransaction();
+    await transaction.query('INSERT INTO users (id, name, cpf, password) VALUES (?, ?, ?, ?)', [id, name, cpf, password]);
+    await transaction.query('INSERT INTO accounts (account_number, balance, user_id) VALUES (?, ?, ?)', [accountNumber, 0, id]);
+
+    await transaction.commit();
+    return { id, account: accountNumber };
   } catch (error) {
+    await transaction.rollback();
     throw new AppError(error.message, 500);
+  } finally {
+    if (transaction) transaction.release();
   }
 };
 
