6350bdf471b092c8b0449048ed6fb14bd3dab920
laecio silva <laecio12silva@gmail.com>
2022-07-17 18:53:15 -0300

    feat: Funções para ser possível fazer um investimento.

---
+++ src/models/Investment/index.js
@@ -0,0 +1,53 @@
+import AppError from '../../errors/AppError.js';
+import connection from '../connection.js';
+
+const purchase = async (id, accountNumber, userId, symbol, quantity, total, averagePrice) => {
+  const transaction = await connection.getConnection();
+  try {
+    transaction.beginTransaction();
+    await transaction.query('UPDATE accounts SET balance = balance - ? WHERE account_number = ?', [total, accountNumber]);
+    await transaction.query('INSERT INTO wallets (id, user_id, investment_symbol, quantity, average_price) VALUES (?, ?, ?, ?, ?)', [id, userId, symbol, quantity, averagePrice]);
+    await transaction.commit();
+  } catch (error) {
+    await transaction.rollback();
+    throw new AppError(error.message, 500);
+  } finally {
+    if (transaction) transaction.release();
+  }
+};
+
+const InvestmentAdd = async (id, accountNumber, quantity, total, averagePrice) => {
+  const transaction = await connection.getConnection();
+  try {
+    transaction.beginTransaction();
+    await transaction.query('UPDATE accounts SET balance = balance - ? WHERE account_number = ?', [total, accountNumber]);
+    await transaction.query('UPDATE wallets SET quantity = quantity + ?, average_price = ? WHERE id = ?', [quantity, averagePrice, id]);
+    await transaction.commit();
+  } catch (error) {
+    await transaction.rollback();
+    throw new AppError(error.message, 500);
+  } finally {
+    if (transaction) transaction.release();
+  }
+};
+
+const getBySymbol = async (symbol) => {
+  try {
+    const [investment] = await connection.execute('SELECT * FROM investments WHERE symbol = ?', [symbol]);
+    return investment;
+  } catch (error) {
+    throw new AppError(error.message, 500);
+  }
+};
+
+const getInvestmentBySymbolByUser = async (userId, symbol) => {
+  try {
+    const [investment] = await connection.execute('SELECT * FROM wallets WHERE user_id = ? AND investment_symbol = ?', [userId, symbol]);
+    return investment;
+  } catch (error) {
+    throw new AppError(error.message, 500);
+  }
+};
+export default {
+  purchase, getBySymbol, InvestmentAdd, getInvestmentBySymbolByUser,
+};
