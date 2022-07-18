54def2f0c5b5400b254c2ad2ea277a0408ca3849
laecio silva <laecio12silva@gmail.com>
2022-07-18 07:33:10 -0300

    feat: Adicionando a rota /vender

--- src/routes/investment.routes.js
+++ src/routes/investment.routes.js
@@ -4,5 +4,6 @@ import investmentController from '../controllers/investment/index.js';
 const investmentRouter = express.Router();
 
 investmentRouter.post('/comprar', investmentController.purchase);
+investmentRouter.post('/vender', investmentController.sell);
 
 export default investmentRouter;
