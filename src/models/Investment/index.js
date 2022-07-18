7ab8919241c25809b560a1c0ed325d28ac3c9739
laecio silva <laecio12silva@gmail.com>
2022-07-18 07:31:45 -0300

    feat: Adicionando a função sell para realizar uma venda atualizar o saldo da pessoa usuária e atualizar a quantidade disponível na corretora.

--- src/models/Investment/index.js
+++ src/models/Investment/index.js
@@ -33,6 +33,30 @@ const InvestmentAdd = async (id, accountNumber, quantity, total, averagePrice, s
   }
 };
 
+const sell = async (id, accountNumber, quantity, total, averagePrice, symbol) => {
+  const transaction = await connection.getConnection();
+  try {
+    transaction.beginTransaction();
+    await transaction.query('UPDATE accounts SET balance = balance + ? WHERE account_number = ?', [total, accountNumber]);
+    await transaction.query('UPDATE investments SET quantity = quantity + ? WHERE symbol = ?', [quantity, symbol]);
+    await transaction.query('UPDATE transactions SET quantity = quantity - ?, average_price = ? WHERE id = ?', [quantity, averagePrice, id]);
+    await transaction.commit();
+  } catch (error) {
+    await transaction.rollback();
+    throw new AppError(error.message, 500);
+  } finally {
+    if (transaction) transaction.release();
+  }
+};
+
+const deleteTransaction = async (id) => {
+  try {
+    await connection.execute('DELETE FROM transactions WHERE id = ?', [id]);
+  } catch (error) {
+    throw new AppError(error.message, 500);
+  }
+};
+
 const getBySymbol = async (symbol) => {
   try {
     const [investment] = await connection.execute('SELECT * FROM investments WHERE symbol = ?', [symbol]);
@@ -51,5 +75,5 @@ const getInvestmentBySymbolByUser = async (userId, symbol) => {
   }
 };
 export default {
-  purchase, getBySymbol, InvestmentAdd, getInvestmentBySymbolByUser,
+  purchase, getBySymbol, InvestmentAdd, getInvestmentBySymbolByUser, sell, deleteTransaction,
 };
