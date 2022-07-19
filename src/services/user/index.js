c9dd33c61426c3edac953f162d9ceac260700599
laecio silva <laecio12silva@gmail.com>
2022-07-19 13:57:00 -0300

    feat: Criptografando a senha antes de salvar no bannco.

--- src/services/user/index.js
+++ src/services/user/index.js
@@ -1,5 +1,5 @@
 import { v4 as uuid } from 'uuid';
-
+import bcrypt from 'bcryptjs';
 import AppError from '../../errors/AppError.js';
 import UserModel from '../../models/User/index.js';
 import generateJWT from '../../utils/generateJWT.js';
@@ -9,8 +9,11 @@ const create = async (name, cpf, password) => {
   const [userExists] = await UserModel.findByCpf(cpf);
 
   if (userExists) throw new AppError('Usuário já existe', 409);
+
+  const hashPassword = await bcrypt.hash(password, 8);
+
   const { id, account } = await UserModel
-    .create(uuid(), name, cpf, password, generateNumberAccount());
+    .create(uuid(), name, cpf, hashPassword, generateNumberAccount());
 
   const token = await generateJWT({ id });
 
