8f00a3922afbc0768c8e09cdc4d85e584ba9f7b0
laecio silva <laecio12silva@gmail.com>
2022-07-21 11:34:19 -0300

    feat: Adicionando a validação do CPF.

--- src/middlewares/user/validate.js
+++ src/middlewares/user/validate.js
@@ -1,4 +1,5 @@
 import AppError from '../../errors/AppError.js';
+import validateCPF from '../../utils/validateCPF.js';
 
 const validate = (request, _response, next) => {
   const { Nome: name, CPF: cpf, Senha: password } = request.body;
@@ -6,6 +7,7 @@ const validate = (request, _response, next) => {
   if (!name) throw new AppError('Nome é obrigatório!');
   else if (!cpf) throw new AppError('CPF é obrigatório!');
   else if (!password) throw new AppError('Senha é obrigatória!');
+  validateCPF(cpf);
 
   next();
 };
