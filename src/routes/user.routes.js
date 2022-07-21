41988f6dc0ff07b62615afcd9bafa04c8adbde72
laecio silva <laecio12silva@gmail.com>
2022-07-21 11:36:44 -0300

    feat: Adicionando  a validação  da session na rota.

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,11 +3,12 @@ import userController from '../controllers/user/index.js';
 import walletController from '../controllers/wallet/index.js';
 import validate from '../middlewares/user/validate.js';
 import ensureAuthenticated from '../middlewares/user/ensureAuthenticated.js';
+import validateSessionData from '../middlewares/user/validateSessionData.js';
 
 const userRouter = express.Router();
 
 userRouter.post('/', validate, userController.create);
-userRouter.post('/session', userController.createSession);
+userRouter.post('/session', validateSessionData, userController.createSession);
 userRouter.get('/ativos', ensureAuthenticated, walletController.getInvestmentsByUser);
 userRouter.get('/ativos/:CodAtivo', ensureAuthenticated, walletController.getInvestmentBySymbolByUser);
 
