e6adc418cb1417390a452c74385bb5208803fa8a
laecio silva <laecio12silva@gmail.com>
2022-07-21 10:43:28 -0300

    refactor: Alterando o parâmetro de "value" para Valor.

--- src/middlewares/account/index.js
+++ src/middlewares/account/index.js
@@ -1,10 +1,10 @@
 import AppError from '../../errors/AppError.js';
 
 const validateValue = (req, res, next) => {
-  const { value } = req.body;
-  if (!value) throw new AppError('Valor é obrigatório');
-  if (typeof value === 'string') throw new AppError('Valor informado deve ser um número', 422);
-  if (value < 0) throw new AppError('Valor informado deve ser maior que 0', 422);
+  const { Valor: value } = req.body;
+  if (value <= 0) throw new AppError('Valor informado deve ser maior que 0.00!', 422);
+  if (!value) throw new AppError('Valor é obrigatório!');
+  if (typeof value === 'string') throw new AppError('Valor informado deve ser um número!', 422);
   next();
 };
 
