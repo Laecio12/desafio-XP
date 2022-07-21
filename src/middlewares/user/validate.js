8894b81d9507a238e9b5866d92a6c0c136e9ee43
laecio silva <laecio12silva@gmail.com>
2022-07-21 10:45:20 -0300

    refactor: Alterando o parâmetros de "name" para "Nome", "cpf" para "CPF" e "password" para "Senha" .

--- src/middlewares/user/validate.js
+++ src/middlewares/user/validate.js
@@ -1,11 +1,11 @@
 import AppError from '../../errors/AppError.js';
 
 const validate = (request, _response, next) => {
-  const { name, cpf, password } = request.body;
+  const { Nome: name, CPF: cpf, Senha: password } = request.body;
 
-  if (!name) throw new AppError('Nome é obrigatório');
-  else if (!cpf) throw new AppError('CPF é obrigatório');
-  else if (!password) throw new AppError('Senha é obrigatória');
+  if (!name) throw new AppError('Nome é obrigatório!');
+  else if (!cpf) throw new AppError('CPF é obrigatório!');
+  else if (!password) throw new AppError('Senha é obrigatória!');
 
   next();
 };
