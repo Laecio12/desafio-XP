08e683298111ef0c90a3b05d3895905aa8f33602
laecio silva <laecio12silva@gmail.com>
2022-07-22 12:06:22 -0300

    feat: Adicionando a rota editar

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,11 +4,13 @@ import walletController from '../controllers/wallet/index.js';
 import validate from '../middlewares/user/validate.js';
 import ensureAuthenticated from '../middlewares/user/ensureAuthenticated.js';
 import validateSessionData from '../middlewares/user/validateSessionData.js';
+import validateEditar from '../middlewares/user/validateEdit.js';
 
 const userRouter = express.Router();
 
 userRouter.post('/', validate, userController.create);
 userRouter.post('/login', validateSessionData, userController.createSession);
+userRouter.put('/editar', ensureAuthenticated, validateEditar, userController.edit);
 userRouter.get('/ativos', ensureAuthenticated, walletController.getInvestmentsByUser);
 userRouter.get('/ativos/:CodAtivo', ensureAuthenticated, walletController.getInvestmentBySymbolByUser);
 
