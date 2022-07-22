26dfddc95c439e1db95bf59b7f6faacef83d23c5
laecio silva <laecio12silva@gmail.com>
2022-07-22 16:25:12 -0300

    feat: Adicionando a função addInvestment onde é possível adicionar um novo ativo.

--- src/services/ administrative/index.js
+++ src/services/ administrative/index.js
@@ -1,8 +1,16 @@
+import AppError from '../../errors/AppError.js';
 import AdministrativeModel from '../../models/ Administrative/index.js';
+import InvestmentModel from '../../models/Investment/index.js';
 
 const getAllUsers = async () => {
   const users = AdministrativeModel.getAllUsers();
   return users;
 };
 
-export default { getAllUsers };
+const addInvestment = async (symbol, price, quantity) => {
+  const [investment] = await InvestmentModel.getBySymbol(symbol);
+  if (investment) throw new AppError(`O ativo ${symbol}  já está cadastrado!`, 404);
+  await AdministrativeModel.addInvestment(symbol, price, quantity);
+};
+
+export default { getAllUsers, addInvestment };
