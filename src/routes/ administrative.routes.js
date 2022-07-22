fde0d0c5060d974e78b4eda3e93dc0f77b6c610c
laecio silva <laecio12silva@gmail.com>
2022-07-22 16:28:04 -0300

    feat: Adicionando a rota /investimentos/adicionar

--- src/routes/ administrative.routes.js
+++ src/routes/ administrative.routes.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import administrative from '../controllers/ administrative/index.js';
+import validateInvestment from '../middlewares/investment/validateInvestment.js';
 
 const administrativeRouter = express.Router();
 
 administrativeRouter.get('/clientes', administrative.getAllUsers);
+administrativeRouter.post('/investimentos/adicionar', validateInvestment, administrative.addInvestment);
 
 export default administrativeRouter;
