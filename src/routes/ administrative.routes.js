060ca8ed61ab2d5a884963cdd434f6820334f93c
laecio silva <laecio12silva@gmail.com>
2022-07-22 17:35:52 -0300

    feat: Adicionado a rota /investimentos/editar/:CodAtivo

--- src/routes/ administrative.routes.js
+++ src/routes/ administrative.routes.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import administrative from '../controllers/ administrative/index.js';
 import validateInvestment from '../middlewares/investment/validateInvestment.js';
+import validateValue from '../middlewares/investment/validateValue.js';
 
 const administrativeRouter = express.Router();
 
 administrativeRouter.get('/clientes', administrative.getAllUsers);
 administrativeRouter.post('/investimentos/adicionar', validateInvestment, administrative.addInvestment);
+administrativeRouter.patch('/investimentos/editar/:CodAtivo', validateValue, administrative.editPrice);
 
 export default administrativeRouter;
