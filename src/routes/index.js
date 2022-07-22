ef6149fdde1f1afbed6a027bf8a98544186fb68b
laecio silva <laecio12silva@gmail.com>
2022-07-22 15:20:09 -0300

    feat: Adicionando a rota admin.

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,7 @@
 import express from 'express';
+import ensureAdmin from '../middlewares/administrative/ensureAdmin.js';
 import ensureAuthenticated from '../middlewares/user/ensureAuthenticated.js';
+import administrativeRouter from './ administrative.routes.js';
 import accountRouter from './accounts.routes.js';
 import investmentRouter from './investment.routes.js';
 import userRouter from './user.routes.js';
@@ -9,5 +11,6 @@ const router = express.Router();
 router.use('/clientes', userRouter);
 router.use('/conta', ensureAuthenticated, accountRouter);
 router.use('/investimentos', investmentRouter);
+router.use('/admin', ensureAuthenticated, ensureAdmin, administrativeRouter);
 
 export default router;
